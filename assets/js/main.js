7b9c1b28c42df971f505d3ad80ed58bd21c1fb5f
Indah_ra <indahra04always@gmail.com>
2024-03-27 14:13:16 +0700

    feat: form page

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/*===== FOCUS =====*/
+const inputs = document.querySelectorAll(".form__input")
+
+/*=== Add focus ===*/
+function addfocus(){
+    let parent = this.parentNode.parentNode
+    parent.classList.add("focus")
+}
+
+/*=== Remove focus ===*/
+function remfocus(){
+    let parent = this.parentNode.parentNode
+    if(this.value == ""){
+        parent.classList.remove("focus")
+    }
+}
+
+/*=== To call function===*/
+inputs.forEach(input=>{
+    input.addEventListener("focus",addfocus)
+    input.addEventListener("blur",remfocus)
+})
+
+function konfirmasi() {
+   
+    var hasil = confirm("Apakah Anda yakin ingin melanjutkan?");
+    
+   
+    if (hasil) {
+        
+        alert("Anda memilih OK!");
+    } else {
+       
+        alert("Anda memilih Cancel!");
+    }
+}
